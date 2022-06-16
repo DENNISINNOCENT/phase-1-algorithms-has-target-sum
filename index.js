689dcd192e77b44224e2130f7c03b3bdf73743fe
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-16 19:37:32 +0300

    commit tree d594b2ae25ca0f4088219c4b4d65a26f0abf7daf

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+  for(let i = 0;i < array.length;i++){
+    let total = target-array[i]
+    for(let j =i + 1;j< array.length;j++){
+      if(total === array[j])
+        return true
+      
+    
+      }
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
